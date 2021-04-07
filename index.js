d775401c6827b5f906c0a3196146d18e3152e58b
celeste.tech@icloud.com <celeste.tech@icloud.com>
2021-04-07 16:05:12 -0500

    commit tree 32fcf9a7139e9eb12eb998bda65d7c6776b5e917

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+
+const HQ = 42
+function distanceFromHqInBlocks(pickupLocation) {
+    if (pickupLocation >= 42) {
+        return pickupLocation - HQ;
+    }
+    else {
+        return HQ - pickupLocation 
+    }
+};
+distanceFromHqInBlocks(50);
+
+
+//distanceFromHqInFeet()
+
+
+//distanceTravelledInFeet()
+
+
+//calculatesFarePrice(start, destination)
